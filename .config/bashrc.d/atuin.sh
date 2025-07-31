7a8b9014717fd2cf9472ec354e3158f07b91e2a6
MadCrabCyder <madcrabcyder@outlook.com>
2025-07-31 17:34:55 +1000

    Updated atuin bootstrap

--- .config/bashrc.d/atuin.sh
+++ .config/bashrc.d/atuin.sh
@@ -1,7 +1,16 @@
 # boot strap for atuin - magical shell history
 # https://github.com/atuinsh/atuin
 
-. "$HOME/.atuin/bin/env"
+# add binaries to PATH if they aren't added yet
+# affix colons on either side of $PATH to simplify matching
+case ":${PATH}:" in
+    *:"$HOME/.atuin/bin":*)
+        ;;
+    *)
+        # Prepending path in case a system-installed binary needs to be overridden
+        export PATH="$HOME/.atuin/bin:$PATH"
+        ;;
+esac
 
 eval "$(atuin init bash)"
 
